da7f7369273f5bac482b0e9c06135940c9ac8548
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-12-16 14:21:14 +0330

    file update

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -10,9 +10,6 @@ if (window.innerWidth < 768) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	if ('AOS' in window) {
-		AOS.init();
-	}
 
 	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
 	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
